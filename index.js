c690b0d77aa31db4708f933390ae91b4529fc364
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-08-25 13:22:33 -0500

    add basic starter packages and server startup code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+  app = express(),
+  cors = require("cors"),
+  bodyParser = require("body-parser");
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//routes go here
+
+//error handling if no routes match
+app.use(function(req,res,next){
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, function(){
+  console.log(`Server starting on port ${PORT}.`);
+});
