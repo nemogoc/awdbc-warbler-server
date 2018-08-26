0d6dc2223046ccbcd163b5686738cadeb6212522
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-08-26 09:29:00 -0500

    add signup function/route

---
+++ handlers/auth.js
@@ -0,0 +1,34 @@
+const db = require("../models");
+const jwt = require("jsonwebtoken");
+
+exports.signin = function(){};
+
+exports.signup = async function(req, res, next){
+  try {
+    let user = await db.User.create(req.body);
+    let { id, username, profileImageUrl } = user;
+    let token = jwt.sign({
+      id,
+      username,
+      profileImageUrl
+    }, process.env.SECRET_KEY
+    );
+    return res.status(200).json({
+      id,
+      username,
+      token
+    });
+    //create a user
+    //sign a token for that user
+    //process.env.SECRET_KEY
+  }
+  catch (err){
+    if(err.code === 11000){ //mongoose "duplicate document" error code
+      err.message = "Sorry, that username and/or email is taken"
+    }
+    return next({
+      status: 400,
+      message: err.message
+    });
+  }
+};
