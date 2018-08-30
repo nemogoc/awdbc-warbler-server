10e1ded1344574756a150ff84fcf60817b4d948f
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-08-30 13:06:20 -0500

    add get all messages route/handler

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes); //<base>/api/auth/<anything> uses routes in authRoutes
 app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
 
+app.use("/api/messages", loginRequired, async function(req, res, next){
+  try {
+    let messages = await db.Message.find().sort({createdAt: "desc" }).populate("user", {
+      username: true,
+      profileImageUrl: true
+    });
+    return res.status(200).json(messages);
+  }
+  catch(err){ return next(err);}
+});
+
 //error handling if no routes match
 app.use(function(req,res,next){
   let err = new Error("Not Found");
