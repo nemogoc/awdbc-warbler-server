e479a233e145beb3e3f312044b9131a102686019
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-08-30 13:06:11 -0500

    add timestamps to messages

--- models/message.js
+++ models/message.js
@@ -2,25 +2,29 @@ const mongoose = require("mongoose"),
   user = require("./user");
 
 const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    maxLength: 160
+    text: {
+      type: String,
+      required: true,
+      maxLength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+  {
+    timestamps: true
   }
-});
+);
 
-messageSchema.pre("remove", async function (next){
+messageSchema.pre("remove", async function (next) {
   try {
     let user = await User.findById(this.user);
     user.messages.remove(this.id);
     await user.save();
     return next();
   }
-  catch(err){
+  catch (err) {
     return next(err);
   }
 });
